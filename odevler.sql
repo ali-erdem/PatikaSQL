a32e04c394a3477194c899f54fd2e2bdfd5bc1e9
ali-erdem <60155664+ali-erdem@users.noreply.github.com>
2026-09-14 21:23:14 +0300

    Update and rename odev1.sql to odevler.sql

--- odevler.sql
+++ odevler.sql
@@ -1,5 +1,14 @@
+--Birinci Ödev
 select title, description from film;
 select * from film where length >60 and length<75;
 select * from film where rental_rate = 0.99 and (replacement_cost=12.99 or replacement_cost = 28.99) ;
 select last_name from customer where first_name = 'Mary';
 select * from film  where length <=50 and (rental_rate != 2.99 and rental_rate != 4.99 );
+
+--İkinci Ödev
+select * from film where replacement_cost >= 12.99 and replacement_cost < 16.99;
+select first_name, last_name from actor where first_name in ('Penelope','Nick','Ed');
+select * from film where rental_rate in (0.99, 2.99, 4.99) and replacement_cost in (12.99, 15.99, 28.99);
+
+--Üçüncü Ödev
+
